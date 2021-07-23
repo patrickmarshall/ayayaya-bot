0abe2b07dde6d6b80216740a7dc74783bfa97d16
Patrick Marshall <patrickmarshall@outlook.sg>
2021-07-23 19:14:54 +0700

    initial commit. with hears feature and forward video feature

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const { Telegraf } = require('telegraf')
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.command('start', ctx => {
+    if (ctx.from.id != process.env.MY_ACCOUNT) {
+        bot.telegram.sendMessage(ctx.chat.id, "Lu siapa anjeng", {})
+    } else {
+        console.log("hi pat")
+    }
+})
+
+bot.command('who', ctx => {
+    bot.telegram.sendMessage(ctx.chat.id, `${ctx.message.from.first_name} ${ctx.message.from.last_name}`, {})
+})
+
+bot.hears((msg,ctx) => {
+    if (ctx.chat.id == process.env.GROUP_ANABEL_BAPAK_BAPAK) {
+        // anabel bapak bapak
+    } else {
+        if (ctx.from.id != process.env.MY_ACCOUNT) {
+            bot.telegram.sendMessage(ctx.chat.id, "Lu siapa anjeng", {})
+        } else {
+            console.log("hi pat")
+        }
+    }
+})
+
+bot.on('video', ctx => {
+    console.log(ctx)
+    bot.telegram.forwardMessage(process.env.GROUP_J, ctx.chat.id, ctx.message.message_id, {})
+})
+
+bot.launch()
