f0d707967871afe600ee1ae2ea46f0a0890b5714
Patrick Marshall <patrickmarshall@Patricks-MacBook-Pro.local>
2021-12-06 22:12:30 +0700

    update help

--- features/greetings.js
+++ features/greetings.js
@@ -14,6 +14,8 @@ function help(ctx) {
     `/reminder - nyalain Manchester United match reminder\n` +
     `/nextfixture - pertandingan Manchester United terdekat\n` +
     `/playgame - dapatkan game game menarik\n` +
+    `/lastmatch - hasil pertandingan terbaru Manchester United\n`+
+    `/matchupdates - nyalain update hasil pertandingan Manchester United\n`+
     `/help - this\n\n` +
 
     `Kamu juga bisa kirim foto, video, gif, etc. nanti aku bakal kirim balik ke kalian`
