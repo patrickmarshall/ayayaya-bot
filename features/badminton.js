6416250820786b157391f7aa77ce2330c1934ad0
Patrick <patrickmarshall@outlook.jp>
2024-06-19 12:03:10 +0700

    fix header

--- features/badminton.js
+++ features/badminton.js
@@ -72,23 +72,20 @@ async function getResult(indonesiaOnly = true) {
     const options = {
         method: 'GET',
         headers: {
-            'accept': '*/*',
-            'accept-language': 'en-US,en;q=0.9,fr;q=0.8,en-GB;q=0.7,id;q=0.6',
-            'cookie': 'cf_clearance=kM0VqySp0NEee9HzCCdFiJuJSg_lYCDK_CoRgo6yxFo-1714630809-1.0.1.1-LQbMMeBq9ajmJ8FVWNRccXt07xRMvIvVkP6qEynqbEOcrs57DYXhf0F_1tKYudzeBGSt40SUvtV6sECSsm_Odw; CookieConsent={stamp:%27vejD+j5aI23Za3L0v37g6Cyd7mJKxjCChlh35hqStRkj2W/iZ/MzJQ==%27%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cmethod:%27explicit%27%2Cver:1%2Cutc:1714630819702%2Cregion:%27id%27}; _gid=GA1.2.964804785.1717084527; cf_clearance=F1Ry1s5d.ERc7eKvCNHPk1R4vKbOv1O.xGeKhIpkM2w-1717119722-1.0.1.1-vxn3t.T5NInrMUiCCy0lUXgZHkvxbZzXos8C1ieDPWLEXIDEuaARo1MkVlUB18mrucDKiKKklZqAAKr9Ke5j8w; _ga=GA1.2.1337385662.1714630820; _ga_HMB879WPYW=GS1.1.1717119717.4.1.1717119733.0.0.0',
-            'priority': 'u=1, i',
-            'referer': url,
-            'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"macOS"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
-            'x-requested-with': 'XMLHttpRequest'
-        }
+            "accept": "*/*",
+            "sec-ch-ua": "\"Google Chrome\";v=\"125\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"macOS\"",
+            "x-requested-with": "XMLHttpRequest",
+            "Referer": url,
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+          }
     };
 
     try {
+        if (url === null) {
+            return [];
+        }
         const response = await fetch(url, options);
         const data = await response.text();
         const jsonData = parseMatchDetails(data);
@@ -210,12 +207,10 @@ function checkMatch() {
                     }
                 }
             });
+        } else {
+            console.log("No match found");
         }
     });
-    // check if in ongoing
-    // if any, delete from ongoing 
-    // should delete from ongoing 
-    // and send message
 }
 
 async function hasilIndonesia(ctx) {
